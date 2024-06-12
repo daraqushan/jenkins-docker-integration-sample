3d4d174157dcc5556ac96645cec658a5e9a19dc8
daraqushan <daraqushan@gmail.com>
2024-06-12 15:07:21 +0530

    third commit

--- src/main/java/cgg/docker/jenkins/App.java
+++ src/main/java/cgg/docker/jenkins/App.java
@@ -4,11 +4,10 @@ package cgg.docker.jenkins;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         System.out.println("testing...");
+        System.out.println("code changed...");
     }
 }
